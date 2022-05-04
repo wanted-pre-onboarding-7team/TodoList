ac135c26ca2e8c2c2f457ab9c6d49a83b89b5007
rlaqudwls25 <rlaqudwls157@gmail.com>
2022-05-04 21:51:08 +0900

    Detail, Update 파일 생성

---
+++ src/components/Detail/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TodoListDetail = () => {
+  return <div>Detail</div>
+}
+
+export default TodoListDetail
---
+++ src/components/Update/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TodoListUpdate = () => {
+  return <div>Update</div>
+}
+
+export default TodoListUpdate
