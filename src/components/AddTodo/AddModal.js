3990f72c396fe7fcd779031f08f2f86004208ecf
wkqkel <89131236+wkqkel@users.noreply.github.com>
2022-05-06 06:53:07 +0900

    feat: 모달창 open 애니메이션 추가

--- src/components/AddTodo/AddModal.js
+++ src/components/AddTodo/AddModal.js
@@ -1,44 +1,58 @@
 import { useState } from 'react'
+import PropTypes from 'prop-types'
 import useTodoList from '../../hooks/useTodoList'
+
+import ExpandBubble from './ExpandBubble'
 
 import styles from './AddModal.module.scss'
 import { CalendarIcon, AddIcon, FlagIcon, MoonIcon } from '../../assets/svgs'
 
-function AddModal() {
+function AddModal({ setOpenAddModal }) {
   const [inputValue, setInputValue] = useState('')
   const { addTodoList } = useTodoList()
-  const handleClose = () => {}
+
+  const handleClose = () => {
+    setOpenAddModal(false)
+  }
 
   const handleInputChange = ({ target: { value } }) => {
     setInputValue(value)
   }
 
   const handleAddBtnClick = () => {
-    if (!inputValue) return
-    // alert eslint에서 눈에 안띈다고 커스텀 alert 사용권장
+    if (!inputValue) return // alert eslint에서 눈에 안띈다고 커스텀 alert 사용권장
     addTodoList(inputValue)
     setInputValue('')
   }
 
   return (
-    <div className={styles.addModal}>
-      <button type='button' className={styles.closeBtn} onClick={handleClose} aria-label='Close button' />
-      <input type='text' value={inputValue} onChange={handleInputChange} placeholder='Enter new task' />
-      <div className={styles.optionWrapper}>
-        <button type='button' className={styles.calendarBtn} aria-label='Calendar button'>
-          <CalendarIcon />
-          <p>Today</p>
-        </button>
-        <button type='button' className={styles.categoryBtn} aria-label='Category button' />
+    <div className={styles.background}>
+      <ExpandBubble />
+      <div className={styles.addModal}>
+        <button type='button' className={styles.closeBtn} onClick={handleClose} aria-label='Close button' />
+        <input type='text' value={inputValue} onChange={handleInputChange} placeholder='Enter new task' />
+        <div className={styles.optionWrap}>
+          <button type='button' className={styles.calendarBtn} aria-label='Calendar button'>
+            <CalendarIcon />
+            <p>Today</p>
+          </button>
+          <div className={styles.categoryWrap}>
+            <button type='button' className={styles.categoryBtn} aria-label='Category button' />
+          </div>
+        </div>
+        <div className={styles.IconWrap}>
+          <AddIcon />
+          <FlagIcon />
+          <MoonIcon />
+        </div>
+        <button type='button' onClick={handleAddBtnClick} className={styles.addBtn} aria-label='Add button' />
       </div>
-      <div className={styles.IconWrapper}>
-        <AddIcon />
-        <FlagIcon />
-        <MoonIcon />
-      </div>
-      <button type='button' onClick={handleAddBtnClick} className={styles.addBtn} aria-label='Add button' />
     </div>
   )
 }
 
 export default AddModal
+
+AddModal.propTypes = {
+  setOpenAddModal: PropTypes.func.isRequired,
+}
