42d9ce75eba3b129a124814160bd2bb2250b6fe1
wkqkel <89131236+wkqkel@users.noreply.github.com>
2022-05-06 14:05:44 +0900

    feat: 카테고리 선택 기능 추가

--- src/components/AddTodo/AddModal.js
+++ src/components/AddTodo/AddModal.js
@@ -4,11 +4,13 @@ import useTodoList from '../../hooks/useTodoList'
 
 import ExpandBubble from './ExpandBubble'
 
+import cx from 'classnames'
 import styles from './AddModal.module.scss'
 import { CalendarIcon, AddIcon, FlagIcon, MoonIcon } from '../../assets/svgs'
 
 function AddModal({ setOpenAddModal }) {
   const [inputValue, setInputValue] = useState('')
+  const [categoryPick, setCategoryPick] = useState('')
   const { addTodoList } = useTodoList()
 
   const handleClose = () => {
@@ -20,9 +22,13 @@ function AddModal({ setOpenAddModal }) {
   }
 
   const handleAddBtnClick = () => {
-    if (!inputValue) return // alert eslint에서 눈에 안띈다고 커스텀 alert 사용권장
-    addTodoList(inputValue)
+    if (!inputValue || !categoryPick) return // alert eslint에서 눈에 안띈다고 커스텀 alert 사용권장
+    addTodoList(inputValue, categoryPick)
     setInputValue('')
+  }
+
+  const handleCategoryClick = (pick) => {
+    setCategoryPick(pick)
   }
 
   return (
@@ -37,7 +43,31 @@ function AddModal({ setOpenAddModal }) {
             <p>Today</p>
           </button>
           <div className={styles.categoryWrap}>
-            <button type='button' className={styles.categoryBtn} aria-label='Category button' />
+            <button
+              type='button'
+              className={cx(
+                styles.categoryBtn,
+                { [styles.pickBusiness]: categoryPick === 'business' },
+                { [styles.pickPersonal]: categoryPick === 'personal' }
+              )}
+              aria-label='Category button'
+            />
+            <button
+              type='button'
+              className={styles.categoryOptionBtn}
+              aria-label='Business button'
+              onClick={() => handleCategoryClick('business')}
+            >
+              <p>business</p>
+            </button>
+            <button
+              type='button'
+              className={styles.categoryOptionBtn}
+              aria-label='Personal button'
+              onClick={() => handleCategoryClick('personal')}
+            >
+              <p>personal</p>
+            </button>
           </div>
         </div>
         <div className={styles.IconWrap}>
--- src/hooks/useTodoList.js
+++ src/hooks/useTodoList.js
@@ -4,11 +4,12 @@ import { todoListState } from '../atom/Todolist'
 const useTodoList = () => {
   const [todoList, setTodoList] = useRecoilState(todoListState)
 
-  const addTodoList = (inputValue) => {
+  const addTodoList = (inputValue, categoryPick) => {
     const newValue = {
       id: todoList.length + 1,
       title: inputValue,
       done: false,
+      category: categoryPick,
     }
     const newTodoList = todoList.concat(newValue)
     setTodoList(newTodoList)
