1126fe02e9e74f2100df1fb38be88444b0e6f266
Leejin-Yang <ejin1996@gmail.com>
2022-05-08 11:00:22 +0900

    style: stylelint 적용

--- src/components/SideBar/SideBarBg.js
+++ src/components/SideBar/SideBarBg.js
@@ -2,7 +2,7 @@ import styles from './SideBarBg.module.scss'
 
 // eslint-disable-next-line react/prop-types
 function SideBarBg({ openSide }) {
-  return <div className={`${styles.circle_button} ${openSide ? styles.circle_scale_up : styles.circle_scale_down}`} />
+  return <div className={`${styles.circleButton} ${openSide ? styles.circleScaleUp : styles.circleScaleDown}`} />
 }
 
 export default SideBarBg
